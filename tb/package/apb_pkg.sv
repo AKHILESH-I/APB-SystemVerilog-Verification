708811cb459f50cae5e85367137d869024dbda2a
Akhilesh Ibrahimpet <akhileshibrahimpet@gmail.com>
2026-05-11 23:29:04 +0530

    Delete tb/package/apb_pkg.sv

--- tb/package/apb_pkg.sv
+++
@@ -1,9 +0,0 @@
-package apb_pkg;
-  `include "APB_TRANS.sv"
-  `include "APB_GEN.sv"
-  `include "APB_DRV.sv"
-  `include "APB_MON.sv"
-  `include "APB_SB.sv"
-  `include "APB_ENV.sv"
-  `include "APB_TEST.sv"
-endpackage
